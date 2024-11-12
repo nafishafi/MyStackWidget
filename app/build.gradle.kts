80e7591c6cde6f5e9aa2c1be2616cadee5c06916
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-11-13 00:13:49 +0700

    finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.dicoding.mystackwidget"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dicoding.mystackwidget"
